c8e5aa367d8ddac041501fbfe70bdc7ff5e6dee8
Pablo Marze <pabloversion1.0@gmail.com>
2020-04-20 22:32:54 -0600

    Segundo parcial

---
+++ Segundo_parcial/Biseccion.c
@@ -0,0 +1,12 @@
+/*
+Autor: pmarze
+Compilador: 9.2.1
+Compilado: gcc Biseccion.c -o Biseccion
+Fecha: lun abr 20 22:29:32 CST 2020
+librerias: stdio, math.h
+Resumen: Se encuentra la raíz de una ecuación por medio del método de bisección.
+Entrada:  Ninguna
+Salida: Gráfica de la ecuación
+*/
+
+#include <stdio.h>
---
+++ Segundo_parcial/Minimos_cuadrados.c
@@ -0,0 +1,12 @@
+/*
+Autor: pmarze
+Compilador: 9.2.1
+Compilado: gcc Minimos_cuadrados.c -o Minimos_cuadrados
+Fecha: lun abr 20 22:26:49 CST 2020
+librerias: stdio, mat.h
+Resumen: El programa genera una gráfica que compara los valores tabulados y la recta que mejor aproxima el crecimiento de los datos ingresados. Estima el peso de cualquier persona en base a su altura
+Entrada:  altura de una persona
+Salida: El peso de esa persona
+*/
+
+#include <stdio.h>
