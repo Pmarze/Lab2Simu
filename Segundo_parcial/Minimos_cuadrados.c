96b1b92efec796b5c4486c2d1a5397e42ba55654
Pablo Marze <pabloversion1.0@gmail.com>
2020-04-26 15:17:23 -0600

    Finalizado

--- Segundo_parcial/Minimos_cuadrados.c
+++ Segundo_parcial/Minimos_cuadrados.c
@@ -23,6 +23,11 @@ float mr,br;            // Definimos como variables globales por practicidad mr 
 //{Estatura, Peso} 	// Se trabaja como un array para generalizar, si se desea analizar otro
 			// conjunto de datos, es posible agregando un arreglo con la misma forma
 float persona[12][2]={{70,155},{63,150},{72,180},{60,135},{66,156},{70,168},{74,178},{65,160},{62,132},{67,145},{65,139},{68,152}};
+
+//{m->inch,inch->m},{m->ft,ft->m},{m->yd,yd->m}, si se necesita convertir a otras unidades solo
+float conv[3][2]={{39.3701,0.0254},{3.2808,0.3048},{1.0936,0.9144}};	// Se selecciona la coordenada correcta
+
+void metros();		// Función para convertir nuestros datos de pulgadas a metros
 float m();		// Función m para calcular la pendiente
 float b1();		// Función b1 para calcular sumatoria de y_i/12
 float b2();		// Función b2 para calcular sumatoria de x_i/12
@@ -31,10 +36,11 @@ float b2();		// Función b2 para calcular sumatoria de x_i/12
 			// más complicado.
 
 void main(){
+    metros(persona,conv);		// Convertimos las estaturas de persona a metros
     FILE *pf;				// Definimos el puntero de nuestro archivo
     char* nombre= "min_cuadrados";	// Llamamos a nuestro archivo min_cuadrados
     char* modo= "wt";			// Estamos trabajando en modo escritura	
-
+    
     if((pf=fopen(nombre,modo))==NULL){	// Si hay un error de lectura se detiene el proceso
         puts("Problemas de escritura");
     }
@@ -50,7 +56,7 @@ void main(){
   
         int i=0;			
         while(i<12){    		// Se escribe en el archivo min_cuadrados cada punto (x,y) de "persona"
-            fprintf(pf,"%.2f  %.2f \n",persona[i][0],persona[i][1]);
+            fprintf(pf,"%.2f  %.2f %.2f %.2f \n",persona[i][0],persona[i][1],0.1,0.1); //también añadimos incertezas
             i++;			// El ciclo debe repetirse 12 veces, i+1 para que el ciclo avance
         } 
 
@@ -64,13 +70,16 @@ void main(){
   					// El comando fprintf(p,"..."); permite escribir comandos en gnuplot
     fprintf(p,"reset \n");		// Borramos cualquier configuración previa que pudiera tener gnuplot
     fprintf(p,"set title 'Minimos cuadrados' \n");		// Se define el título del gráfico
-    fprintf(p,"set xlabel 'Estatura'\n");	// Se define el título del eje x
-    fprintf(p,"set ylabel 'Peso' \n");		// Se define el título del eje y
-    fprintf(p,"set xrange [55:80] \n");		// Se define el rango del eje x
-    fprintf(p,"set yrange [120:200] \n");	// Se define el rango del eje y
-    fprintf(p,"plot %f*x+(%f), 'min_cuadrados' \n",mr,br);	// Se plotea simultáneamente la línea que mejor
+    fprintf(p,"set xlabel 'Estatura (m)' \n");	// Se define el título del eje x
+    fprintf(p,"set ylabel 'Peso (Lb)' \n");	// Se define el título del eje y
+    fprintf(p,"set term png \n");		// Defnimos la terminal como png, para crear un gráfico de los datos
+    fprintf(p,"set key bottom right \n");	// Cambiamos la posición de key
+    fprintf(p,"set output 'minimos.png' \n");   // Guardamos la imagen obtenida con el nombre minimos.png
+    fprintf(p,"plot %f*x+(%f), 'min_cuadrados' with xyerrorbars \n",mr,br);	// Se plotea simultáneamente la línea que mejor
     								// se aproxima a nuestros puntos y también
 								// todos los puntos almacenados en min_cuadrados
+    
+    
     pclose(p);				// Se cierra gnuplot y deja nuestra gráfica en pantalla
 }
 
@@ -98,4 +107,11 @@ float b2(float a[12][2]){		// Función para calcular la segunda mitad de b sin e
         b1+=a[i][0];			// b1=sumatoria de xk
     }	
     return b1/12;			// b1/n con n=12
+}
+
+void metros(float a[12][2],float b[3][2]){	// Función para convertir unidades a metros
+    for(int i=0;i<12;i++){	
+        float z=a[i][0]*b[0][1];		// Si se desea convertir a otra unidad solo se debe cambiar
+        a[i][0]=z;				// el valor de la coordenada ej: m->yd  entonces escribimos b[2][0]
+    }
 }
