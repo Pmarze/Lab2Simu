533318f336a80597e48694cb30f895cfd74f2254
Pablo Marze <pabloversion1.0@gmail.com>
2020-04-25 20:49:27 -0600

    avances

--- Segundo_parcial/Biseccion.c
+++ Segundo_parcial/Biseccion.c
@@ -4,9 +4,48 @@ Compilador: 9.2.1
 Compilado: gcc Biseccion.c -o Biseccion
 Fecha: lun abr 20 22:29:32 CST 2020
 librerias: stdio, math.h
-Resumen: Se encuentra la raíz de una ecuación por medio del método de bisección.
+Resumen: Se encuentra la raíz de la función 
+x^3-2x^2-3x+10 por medio del método de bisección.
 Entrada:  Ninguna
 Salida: Gráfica de la ecuación
 */
 
 #include <stdio.h>
+#include <math.h>
+#define EPS 0.000001	
+
+float a=0.5;
+float b=3.0;
+float fun(float);
+float biseccion(float,float);
+
+void main(){
+    printf("raiz del polinomio= %f \n", biseccion(a,b));
+}
+
+
+float fun(float x){
+    float res;
+    res= 
+//    res = pow(x,3)-2*pow(x,2)-3*x+10;
+    return res;
+}
+
+float biseccion(float a, float b){
+    float p=(a+b)/2;
+    int n=0;
+    if (fabs(fun(p))-0<EPS || n<300){
+        printf("Raiz encontrada");
+        return p;
+    }
+    else if (fun(a)*fun(p)>0){
+        printf("%f p por a\n",fun(p));
+        n++;
+        biseccion(p,b);
+    }
+    else{
+        printf("%f p por b\n",fun(p));
+        n++;
+        biseccion(a,p);
+    }
+}
--- Segundo_parcial/Minimos_cuadrados.c
+++ Segundo_parcial/Minimos_cuadrados.c
@@ -4,9 +4,51 @@ Compilador: 9.2.1
 Compilado: gcc Minimos_cuadrados.c -o Minimos_cuadrados
 Fecha: lun abr 20 22:26:49 CST 2020
 librerias: stdio, mat.h
-Resumen: El programa genera una gráfica que compara los valores tabulados y la recta que mejor aproxima el crecimiento de los datos ingresados. Estima el peso de cualquier persona en base a su altura
+Resumen: El programa genera una gráfica que compara los 
+valores tabulados y la recta que mejor aproxima el 
+crecimiento de los datos ingresados. 
+Estima el peso de cualquier persona en base a su altura
 Entrada:  altura de una persona
 Salida: El peso de esa persona
 */
 
 #include <stdio.h>
+#include <math.h>
+#include <stdio_ext.h>
+//{Estatura (pulgadas) , Peso (libras)}
+float persona[12][2]={{70,155},{63,150},{72,180},{60,135},{66,156},{70,168},{74,178},{65,160},{62,132},{67,145},{65,139},{68,152}};
+float m();
+float b();
+
+
+void main(){
+    int n;
+    float pend,con;
+    printf("Ingrese una altura de una persona: ");
+    scanf("%d",&n);
+    __fpurge(stdin);        // Se limpia el buffer para evitar ingreso de datos errados
+    pend=m(persona);
+    con=b(persona);
+    printf("%f,%f \n",pend,con);
+
+}
+
+float m(float a[12][2]){
+    float m1=0, m2=0, m3=0, m4=0;
+    for (int i=0;i<12;i++){
+        m1+=a[i][0]*a[i][1];
+        m2+=a[i][0];
+        m3+=a[i][1];
+        m4+=pow(a[i][0],2);
+    }
+    return (12*m1-(m2*m3))/(12*m4-pow(m2,2));
+}
+
+float b(float a[12][2]){
+    float b1=0, b2=0;
+    for (int i=0;i<12;i++){
+        b1+=a[i][1];
+        b2+=a[i][0];
+    }
+    return (b1-3.2156*b2)/12;
+}
